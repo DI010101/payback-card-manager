4dd3c357bf1e87267d4509d3fdd1e37c30701ab4
DI010101 <dici.handy@gmail.com>
2025-10-03 18:05:10 +0200

    Update CardDao.kt

--- app/src/main/java/com/example/paybackcardmanager/CardDao.kt
+++ app/src/main/java/com/example/paybackcardmanager/CardDao.kt
@@ -1,19 +1,4 @@
 package com.example.paybackcardmanager
 
-import androidx.room.*
-import kotlinx.coroutines.flow.Flow
-
-@Dao
-interface CardDao {
-    @Query("SELECT * FROM cards ORDER BY cardName")
-    fun getAllCards(): Flow<List<Card>>
-    
-    @Insert
-    suspend fun insertCard(card: Card)
-    
-    @Delete
-    suspend fun deleteCard(card: Card)
-    
-    @Query("SELECT * FROM cards WHERE id = :cardId")
-    suspend fun getCardById(cardId: Long): Card?
-}
+// Diese Datei wird nicht mehr verwendet  
+// Wurde ersetzt durch CardManager.kt
