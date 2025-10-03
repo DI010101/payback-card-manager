c65a4b59dbe5679ac2f8a2337e31c9f9d86c1ca8
DI010101 <dici.handy@gmail.com>
2025-10-03 04:44:19 +0200

    Create CardDao.kt

---
+++ app/src/main/java/com/example/paybackcardmanager/CardDao.kt
@@ -0,0 +1,20 @@
+kotlin
+package com.example.paybackcardmanager
+
+import androidx.room.*
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface CardDao {
+    @Query("SELECT * FROM cards ORDER BY cardName")
+    fun getAllCards(): Flow<List<Card>>
+    
+    @Insert
+    suspend fun insertCard(card: Card)
+    
+    @Delete
+    suspend fun deleteCard(card: Card)
+    
+    @Query("SELECT * FROM cards WHERE id = :cardId")
+    suspend fun getCardById(cardId: Long): Card?
+}
