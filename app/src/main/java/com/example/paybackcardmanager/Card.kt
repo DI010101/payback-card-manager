8b924455f61d994308d7a3cedbf177ea53c06ee0
DI010101 <dici.handy@gmail.com>
2025-10-03 10:51:47 +0200

    Update Card.kt

--- app/src/main/java/com/example/paybackcardmanager/Card.kt
+++ app/src/main/java/com/example/paybackcardmanager/Card.kt
@@ -1,11 +1,6 @@
 package com.example.paybackcardmanager
 
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-
-@Entity(tableName = "cards")
 data class Card(
-    @PrimaryKey(autoGenerate = true)
     val id: Long = 0,
     val cardNumber: String,
     val cardName: String,
