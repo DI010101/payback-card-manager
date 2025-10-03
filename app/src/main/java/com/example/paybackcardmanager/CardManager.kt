a53dd021bed5e69c7dd48066529017bea9c8c8b9
DI010101 <dici.handy@gmail.com>
2025-10-03 10:51:10 +0200

    Create CardManager.kt

---
+++ app/src/main/java/com/example/paybackcardmanager/CardManager.kt
@@ -0,0 +1,32 @@
+package com.example.paybackcardmanager
+
+import android.content.Context
+import android.content.SharedPreferences
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+
+class CardManager(private val context: Context) {
+    private val sharedPreferences: SharedPreferences = context.getSharedPreferences("payback_cards", Context.MODE_PRIVATE)
+    private val gson = Gson()
+    
+    fun saveCard(card: Card) {
+        val cards = getCards().toMutableList()
+        cards.add(card.copy(id = System.currentTimeMillis())) // Einfache ID-Generierung
+        saveCards(cards)
+    }
+    
+    fun getCards(): List<Card> {
+        val cardsJson = sharedPreferences.getString("cards", "[]")
+        val type = object : TypeToken<List<Card>>() {}.type
+        return gson.fromJson(cardsJson, type) ?: emptyList()
+    }
+    
+    private fun saveCards(cards: List<Card>) {
+        val cardsJson = gson.toJson(cards)
+        sharedPreferences.edit().putString("cards", cardsJson).apply()
+    }
+    
+    fun getCardById(cardId: Long): Card? {
+        return getCards().find { it.id == cardId }
+    }
+}
